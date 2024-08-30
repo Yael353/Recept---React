315e0fc027d93a729175206301f35d4089752b91
Yaser Elkhabiry <yaser.elkhabiry@outlook.com>
2024-08-30 12:13:30 +0200

    Form samt koppla till App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import Nav from "./components/header/Nav";
+import CreateRecipeForm from "./components/forms/CreateRecipeForm";
 
 function App() {
   return (
     <>
-    <Nav />
-  
-      
+      <Nav />
+      <CreateRecipeForm />
     </>
   );
 }

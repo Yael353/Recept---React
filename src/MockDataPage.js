c23131d6e0de1a875b602f50e7c07e67413940b2
Rebecka <rebeckagamble@hotmail.com>
2024-08-30 12:34:50 +0200

    get mock data

--- src/MockDataPage.js
+++ src/MockDataPage.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 function MockDataPage() {
   return (
-    <div>
-        <h2>This is the page for MockData_page</h2>
+    <div id='MockDataPage'>
+        <h2 className='h-[200px] bg-green-300'>This is the page for MockData</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae similique consectetur eveniet perferendis explicabo ut natus, illo ea quas aliquam fugiat ratione voluptates omnis ipsa magni nostrum quisquam obcaecati voluptate iusto laborum animi. A odit harum sunt eveniet eos architecto dolores vero quibusdam iste libero, laudantium exercitationem optio ad laboriosam sint explicabo deleniti facere? Officia eveniet quam numquam tenetur quia ea omnis architecto, sequi praesentium, nihil facilis repellendus eum sapiente laborum dolore! Accusantium, in deserunt commodi nam, corporis, officia repellendus ea quod perferendis nulla asperiores cum facere assumenda? Tenetur, voluptas excepturi! Accusantium consectetur enim voluptatem fuga consequatur laudantium corporis iure.
+        </p>
 
     </div>
   )
--- src/components/header/Nav.js
+++ src/components/header/Nav.js
@@ -1,16 +1,17 @@
 import React from "react";
 import ApiPage from "../../ApiPage";
 
+
 function Nav() {
   return (
     <nav className="flex flex-row gap-10 h-[60px] bg-pink-300">
       <ul className="flex gap-4">
         <li>
 
-          <a href="">API</a>
+          <a href="#ApiPage">API</a>
         </li>
         <li>
-            <a href="">Mock data</a>
+            <a href="#MockDataPage">Mock data</a>
         </li>
       </ul>
     </nav>

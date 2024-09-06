ffdd43a0ce5512e678ef6b38efde608d0e9d2c7f
Rebecka <rebeckagamble@hotmail.com>
2024-09-06 10:58:53 +0200

    text to english

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,67 +1,79 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Login = ({ onLogin, toggleForm }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    //localStorage 
-    const existingUsers = JSON.parse(localStorage.getItem('users')) || [];
-    const user = existingUsers.find(user => user.username === username && user.password === password);
+    //localStorage
+    const existingUsers = JSON.parse(localStorage.getItem("users")) || [];
+    const user = existingUsers.find(
+      (user) => user.username === username && user.password === password
+    );
 
     //funktion för att logga in om användaren är giltig och finns
     if (user) {
-      localStorage.setItem('currentUser', JSON.stringify(user));
+      localStorage.setItem("currentUser", JSON.stringify(user));
       onLogin();
-      alert('Du är nu inloggad');
+      alert("You are now logged in!");
     } else {
-      alert('Oops! Ogiltigt användarnamn eller lösenord!');
+      alert("Oops! Invalid username or password!");
     }
   };
 
   return (
-    <div className='flex flex-col lg:flex-row justify-center gap-10 lg:justify-between items-center h-[80vh] lg:gap-20'>
-      <div className='flex flex-col items-center lg:items-start gap-2 w-[400px] lg:w-[550px]'>
-        <h1 className='text-2xl lg:text-4xl font-bold'>Välkommen till React--Recept</h1>
-        <h3 className='text-2xl font-semibold italic'>Ett Recept App bygg av grupp: Nr6</h3>
-        <p className='text-md text-center lg:text-xl lg:text-start italic'>Har du inget konto? klicka på "Go to Register knappen" för att sätta igång</p>
-      </div>  
-      <form onSubmit={handleSubmit} className='flex flex-col items-center justify-center gap-4 border rounded-xl w-[400px] lg:w-[500px] h-[400px]'>
-        <h1 className='text-2xl font-semibold'>Login</h1>
-        <p className='italic text-xl font-medium'>Fyll i dina uppgifter</p>
-        <label className='text-xl'>
+    <div className="flex flex-col lg:flex-row justify-center gap-10 lg:justify-between h-screen items-center lg:gap-20">
+      <div className="flex flex-col items-center lg:items-start gap-2 w-[400px] lg:w-[550px]">
+        <h1 className="text-2xl lg:text-4xl font-bold">
+          Welcome to React-Recipes
+        </h1>
+        <h3 className="text-2xl font-semibold italic">
+          A Recipe App built by group Nr6
+        </h3>
+        <p className="text-md text-center lg:text-xl lg:text-start italic">
+          Don't have an account? Click the "Go to Register button" to get
+          started.
+        </p>
+      </div>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col shadow-lg items-center justify-center gap-4 border rounded-xl w-[400px] lg:w-[500px] h-[400px]"
+      >
+        <h1 className="text-2xl font-semibold">Login</h1>
+        <p className="italic text-xl font-medium">Fill in your details</p>
+        <label className="text-xl">
           Username:
           <input
-            className='border rounded-md px-2 ml-5 outline-none p-1'
+            className="border rounded-md px-2 ml-5 outline-none p-1"
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
         </label>
-        <label className='text-xl'>
+        <label className="text-xl">
           Password:
           <input
-            className='border rounded-md px-2 ml-6 outline-none p-1'
+            className="border rounded-md px-2 ml-6 outline-none p-1"
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
         </label>
-        <div className='flex flex-col w-[360px] gap-4'>
-            <button 
-            className='bg-blue-400 w-full p-2 rounded-md text-white'
+        <div className="flex flex-col w-[360px] gap-4">
+          <button
+            className="bg-blue-400 w-full p-2 rounded-md text-white"
             type="submit"
-            >
+          >
             Login
-            </button>
-            <button
-                className='bg-slate-600 text-white p-2 w-full rounded-md'
-                onClick={toggleForm}
-            >
-                Go to Register
-            </button>
+          </button>
+          <button
+            className="bg-slate-600 text-white p-2 w-full rounded-md"
+            onClick={toggleForm}
+          >
+            Go to Register
+          </button>
         </div>
       </form>
     </div>
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,69 +1,78 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Register = ({ onRegister, toggleForm }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const existingUsers = JSON.parse(localStorage.getItem('users')) || [];
-    if (existingUsers.find(user => user.username === username)) {
-      alert('Det angiven konto, finns redan');
+    const existingUsers = JSON.parse(localStorage.getItem("users")) || [];
+    if (existingUsers.find((user) => user.username === username)) {
+      alert("The specified account already exists.");
       return;
     }
 
     const newUser = { username, password };
     existingUsers.push(newUser);
-    localStorage.setItem('users', JSON.stringify(existingUsers));
+    localStorage.setItem("users", JSON.stringify(existingUsers));
 
-    alert('Ditt konto har skapats.');
+    alert("Your account has been created.");
     if (onRegister) onRegister();
   };
 
   return (
-    <div className='flex flex-col lg:flex-row items-center justify-center gap-10 lg:justify-between lg:gap-20 h-[80vh]'>
-        <div>
-            <h1 className='text-3xl text-center lg:text-start font-bold'>Välkomen till register Sida!</h1>
-            <h3 className='text-lg lg:text-xl text-center lg:text-start mt-4 italic'>Här kan du registrera dig grattis och med 3 enkla steg</h3>
-            <p></p>
+    <div className="flex flex-col lg:flex-row items-center justify-center gap-10 lg:justify-between lg:gap-20 h-screen">
+      <div>
+        <h1 className="text-3xl text-center lg:text-start font-bold">
+          Register your account
+        </h1>
+        <h3 className="text-lg lg:text-xl text-center lg:text-start mt-4 italic">
+          Here you can register for free and with 3 simple steps
+        </h3>
+        <p></p>
+      </div>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col items-center justify-center gap-4 border rounded-xl w-[400px] lg:w-[500px] h-[400px]"
+      >
+        <h1 className="text-2xl font-bold">Register</h1>
+        <p className="text-xl italic font-bold">
+          This is a simple form, fill in all fields.
+        </p>
+        <label className="text-xl">
+          Username:
+          <input
+            className="border rounded-md px-2 ml-5 outline-none p-1"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label className="text-xl">
+          Password:
+          <input
+            className="border rounded-md px-2 ml-6 outline-none p-1"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <div className="flex flex-col w-[360px] gap-2">
+          <button
+            className="bg-blue-400 w-full p-2 rounded-md text-white"
+            type="submit"
+          >
+            Register
+          </button>
+          <button
+            className="bg-slate-500 w-full p-2 rounded-md text-white"
+            onClick={toggleForm}
+          >
+            Go to Login
+          </button>
         </div>
-        <form onSubmit={handleSubmit} className='flex flex-col items-center justify-center gap-4 border rounded-xl w-[400px] lg:w-[500px] h-[400px]'>
-            <h1 className='text-2xl font-bold'>Registrera</h1>
-            <p className='text-xl italic font-bold'>Det här en enkel fomrulär, fyll i alla rad!</p>
-            <label className='text-xl'>
-            Username:
-            <input
-                className='border rounded-md px-2 ml-5 outline-none p-1'
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-            />
-            </label>
-            <label className='text-xl'>
-            Password:
-            <input
-                className='border rounded-md px-2 ml-6 outline-none p-1'
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            </label>
-            <div className='flex flex-col w-[360px] gap-2'>
-                <button
-                className='bg-blue-400 w-full p-2 rounded-md text-white'
-                type="submit"
-                >
-                Register
-                </button>
-            <button
-                className='bg-slate-500 w-full p-2 rounded-md text-white'
-                onClick={toggleForm}
-            >
-                Go to Login
-            </button>
-            </div>
-        </form>
+      </form>
     </div>
   );
 };
